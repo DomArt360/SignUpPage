a021ce2494bc0f6d9bb290177b7b6b4a5f2e6e12
Domantas Martišiūnas <domantasmart04@gmail.com>
2025-03-25 11:36:53 +0200

    port added

--- config.php
+++ config.php
@@ -3,8 +3,9 @@ $host="localhost";
 $user="root";
 $password="";
 $database="users_db";
+$port = "3308";
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database, $port);
 if ($conn->connect_error) {
   die("Connection failed: ". $conn->connect_error);
 }
--- login_register.php
+++ login_register.php
@@ -8,12 +8,12 @@ if(isset($_POST['register'])){
   $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
   $role = $_POST['role'];
 
-  $checkEmail = $conn->query("SELECT email FROM users WHERE email = '$email' ");
+  $checkEmail = $conn->query("SELECT email FROM user WHERE email = '$email' ");
   if($checkEmail->num_rows > 0){
     $_SESSION['register_error'] = 'Email is already registered';
     $_SESSION['active_form'] = 'register';
   }else{
-    $conn->query("INSERT INTO users (name,email, password, role) VALUES ('$name','$email', '$password', '$role') ");
+    $conn->query("INSERT INTO user (name,email, password, role) VALUES ('$name','$email', '$password', '$role') ");
   }
 
   header("Location: index.php");
@@ -24,7 +24,7 @@ if(isset($_POST['login'])){
   $email= $_POST['email'];
   $password=$_POST['password'];
 
-  $result = $conn->query("SELECT * FROM users WHERE email = '$email'");
+  $result = $conn->query("SELECT * FROM user WHERE email = '$email'");
   if($result->num_rows > 0){
     $user = $result ->fetch_assoc();
     if(password_verify($password,$user['password'])){
